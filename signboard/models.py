a1e18926f7e4161c885661957a59dd36c7d1df64
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-21 15:43:26 +0700

    First migration

--- signboard/models.py
+++ signboard/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 
 class Line(models.Model):
-    line_number = models.CharField(primary_key=True)
+    line_number = models.CharField(max_length=10, primary_key=True)
 
     def __str__(self):
         return "รถโดยสารสาย {}".format(self.line_number)
 
 
 class Tram(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     line = models.ForeignKey(Line, on_delete=models.CASCADE)
     full_seats = models.IntegerField()
     remaining_seats = models.IntegerField()
