0b5a5f416bcedf447b374acbdaaae4a2087f7437
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-21 15:28:57 +0700

    Models on tram line and car

--- signboard/models.py
+++ signboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Line(models.Model):
+    line_number = models.CharField(primary_key=True)
+
+    def __str__(self):
+        return "รถโดยสารสาย {}".format(self.line_number)
+
+
+class Tram(models.Model):
+    name = models.CharField()
+    line = models.ForeignKey(Line, on_delete=models.CASCADE)
+    full_seats = models.IntegerField()
+    remaining_seats = models.IntegerField()
