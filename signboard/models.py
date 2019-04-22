fe49c7ca3d9992ebd957d933a7bb23fc57d1cdf4
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-22 13:58:15 +0700

    Adding line colour

--- signboard/models.py
+++ signboard/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Line(models.Model):
     line_number = models.CharField(max_length=10, primary_key=True)
+    line_color = models.CharField(max_length=24)
 
     def __str__(self):
         return "รถโดยสารสาย {}".format(self.line_number)
