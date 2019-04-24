ce4845f444755a988668b06e374112ae11c85b6f
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-24 13:43:27 +0700

    Registering SignAlert to admin

--- signboard/admin.py
+++ signboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Line, Tram
+from .models import Line, Tram, SignAlert
 
 # Register your models here.
 admin.site.register(Line)
 admin.site.register(Tram)
+admin.site.register(SignAlert)
