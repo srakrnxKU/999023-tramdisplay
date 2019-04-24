70a24a709a0fe8c824591c755102175b28a93672
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-24 15:03:01 +0700

    Changing model variable

--- signboard/views.py
+++ signboard/views.py
@@ -45,7 +45,7 @@ def set_tram_time(request, id, time):
 
 def set_tram_seats(request, id, seats):
     tram = Tram.objects.get(pk=id)
-    tram.remaining_seats = seats
+    tram.occupied_seats = seats
     tram.save()
     return HttpResponse("done")
 
